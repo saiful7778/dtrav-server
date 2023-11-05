6b2d983f0248f9e645007cf25fb2cb7f48723d85
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-05 21:42:06 +0600

    get cookie from client

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "UPDATE", "OPTIONS", "PATCH"],
-};
-
-app.use(cors({ corsOptions }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://dtrav-travle.web.app/"],
+    methods: ["GET", "POST", "UPDATE", "OPTIONS", "PATCH"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.use(express.json());
 
 const port = process.env.PORT || 5001;
@@ -21,12 +24,22 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/tour_packages", (req, res) => {
+  console.log("Token:", req.cookies.token);
+  res.send({ tour_package: 0 });
+});
+
 app.post("/jwt", async (req, res) => {
   const user = req.body;
   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "1h",
   });
-  res.send(token);
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+    })
+    .send({ success: true });
 });
 
 app.listen(port, () => {
