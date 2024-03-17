3e21c084e144b009e4e965cc6cfcd16b5e59606c
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-17 11:38:13 +0600

    Add mongoose and cors

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authentication from "./src/routes/authentication.js";
 dotenv.config();
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5001;
+// eslint-disable-next-line no-undef
+const dbUrl = process.env.DB_CONNECT;
+
+(async () => {
+  try {
+    console.log("connecting...");
+    await mongoose.connect(dbUrl);
+    console.log("connected DB");
+  } catch (err) {
+    console.log("connection failed");
+    console.log(err);
+  }
+})();
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "DELETE", "OPTIONS", "PATCH"],
+  }),
+);
+app.use(cookieParser());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).send({
@@ -13,6 +38,8 @@ app.get("/", (req, res) => {
     message: "Server is running",
   });
 });
+
+app.use("/authentication", authentication);
 
 app.listen(port, () => {
   console.log("Server is running on port", port);
